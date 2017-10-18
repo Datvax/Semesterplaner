d89f857aaea6b2087e73f1c04aacbc3e5fbef508
Datvax <alexgrill@t-online.de>
2017-10-18 15:56:57 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 18.10.2017
+ * Time: 15:13
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<div id="navigation" >
+
+</div>
+<div id="main">
+	plopp
+</div>
+<footer>
+</footer>
+</body>
+</html>

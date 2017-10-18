32b14fcbe18c7614a8970de65b4f0fd392b1f6e8
Datvax <alexgrill@t-online.de>
2017-10-18 20:39:03 +0200

    added a bunch of files to the project

--- index.php
+++ index.php
@@ -17,7 +17,60 @@
 	<?php include "plugin/navigation/navbar.php"; ?>
 </div>
 <div id="main">
-
+	<table id="timetable">
+		<thead>
+			<tr>
+				<th>
+					Tag
+				</th>
+				<th>
+					1. Semester
+				</th>
+				<th>
+					2. Semester
+				</th>
+				<th>
+					3. Semester
+				</th>
+				<th>
+					4. Semester
+				</th>
+				<th>
+					5. Semester
+				</th>
+				<th>
+					6. Semester
+				</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>
+					Montag
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Dienstag
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Mittwoch
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Donnerstag
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Freitag
+				</td>
+			</tr>
+	</table>
+	</tbody>
 </div>
 <footer>
 </footer>
